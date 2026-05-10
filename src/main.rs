3d321c20418f3b95f3908784d7441e9b83273f50
LukeVerlan <verlangieriluke@gmail.com>
2026-05-09 17:00:36 -0700

    Added RTIC to the project

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 #![no_std]
 #![no_main]
 
-use cortex_m_rt::entry;
 use panic_halt as _;
 
-#[entry]
-fn main() -> !{
-    loop {
-        
+#[rtic::app(device = stm32f4xx_hal::pac, peripherals = true)]
+mod app {
+    use stm32f4xx_hal::prelude::*;
+    #[shared]
+    struct Shared {}
+
+    #[local]
+    struct Local {}
+
+    #[init]
+    fn init(cx: init::Context) -> (Shared, Local) {
+        let rcc = cx.device.RCC.constrain();
+        let _clocks = rcc.cfgr.use_hse(25.MHz()).sysclk(96.MHz()).freeze();
+        (Shared {}, Local {})
     }
 }
